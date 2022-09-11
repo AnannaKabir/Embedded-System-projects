be153a9939d1a89fe1c91856fd0250af14d206cc
Moumita Kabir Ananna <45585390+AnannaKabir@users.noreply.github.com>
2022-09-11 15:07:33 +0600

    LCD Static text

---
+++ MSP-EXP432P4111 Projects/lcd_f_static_text_MSP_EXP432P4111_nortos_ccs/Debug/lcd_f_static_text.d
@@ -0,0 +1,159 @@
+# FIXED
+
+lcd_f_static_text.obj: ../lcd_f_static_text.c
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/driverlib.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/adc14.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+lcd_f_static_text.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp432p4111.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/core_cm4.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_version.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_compiler.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_ccs.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/mpu_armv7.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/system_msp432p4111.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/aes256.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/comp_e.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/cpu.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/crc32.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/cs.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/dma.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/eusci.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/fpu.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/gpio.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/i2c.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/mpu.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pcm.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pmap.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pss.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/ref_a.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/reset.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rom.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rom_map.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rtc_c.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/spi.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/systick.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/timer32.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/timer_a.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/uart.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/wdt_a.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/sysctl_a.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/flash_a.h
+lcd_f_static_text.obj: C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/lcd_f.h
+
+../lcd_f_static_text.c:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/driverlib.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/adc14.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp432p4111.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/core_cm4.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_version.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/inc/system_msp432p4111.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/aes256.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/comp_e.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/cpu.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/crc32.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/cs.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/dma.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/interrupt.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/eusci.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/fpu.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/gpio.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/i2c.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/mpu.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pcm.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pmap.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/pss.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/ref_a.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/reset.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rom.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rom_map.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/rtc_c.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/spi.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/systick.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/timer32.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/timer_a.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/uart.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/wdt_a.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/sysctl_a.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/flash_a.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_40_00_05/source/ti/devices/msp432p4xx/driverlib/lcd_f.h:
+
---
+++ MSP-EXP432P4111 Projects/lcd_f_static_text_MSP_EXP432P4111_nortos_ccs/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p4111.cmd 
+
+C_SRCS += \
+../lcd_f_static_text.c \
+../system_msp432p4111.c 
+
+C_DEPS += \
+./lcd_f_static_text.d \
+./system_msp432p4111.d 
+
+OBJS += \
+./lcd_f_static_text.obj \
+./system_msp432p4111.obj 
+
+OBJS__QUOTED += \
+"lcd_f_static_text.obj" \
+"system_msp432p4111.obj" 
+
+C_DEPS__QUOTED += \
+"lcd_f_static_text.d" \
+"system_msp432p4111.d" 
+
+C_SRCS__QUOTED += \
+"../lcd_f_static_text.c" \
+"../system_msp432p4111.c" 
+
+
