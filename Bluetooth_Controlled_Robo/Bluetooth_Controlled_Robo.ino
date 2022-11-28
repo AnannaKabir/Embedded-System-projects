ed56ce09900415818a6bd8cf239b5f72c318161e
Moumita Kabir Ananna <45585390+AnannaKabir@users.noreply.github.com>
2022-11-28 10:45:45 +0600

    Add files via upload
    Bluetooth Controlled Robot

---
+++ Bluetooth_Controlled_Robo/Bluetooth_Controlled_Robo.ino
@@ -0,0 +1,134 @@
+#define in1 12 
+#define in2 11
+
+#define in3 10
+#define in4 9
+
+
+int command; 
+int Speed = 204; 
+int Speedsec;
+int buttonState = 0;
+int lastButtonState = 0;
+int Turnradius = 0;
+int brakeTime = 45;
+int brkonoff = 1; 
+
+void setup() {
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);  
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    Stop(); 
+    switch (command) {
+      
+      case 'F':
+        forward();
+        break;
+      case 'B':
+        back();
+        break;
+      case 'L':
+        left();
+        break;
+      case 'R':
+        right();
+        break;
+
+      case '0':
+        Speed = 100;
+        break;
+      case '1':
+        Speed = 140;
+        break;
+      case '2':
+        Speed = 153;
+        break;
+      case '3':
+        Speed = 165;
+        break;
+      case '4':
+        Speed = 178;
+        break;
+      case '5':
+        Speed = 191;
+        break;
+      case '6':
+        Speed = 204;
+        break;
+      case '7':
+        Speed = 216;
+        break;
+      case '8':
+        Speed = 229;
+        break;
+      case '9':
+        Speed = 242;
+        break;
+      case 'q':
+        Speed = 255;
+        break;
+    }
+    
+    Speedsec = Turnradius;
+    if (brkonoff == 1) {
+      brakeOn();
+    } else {
+      brakeOff();
+    } 
+  }
+}
+
+void forward() {
+  analogWrite(in1, Speed);
+  analogWrite(in3, Speed);
+}
+
+void back() {
+  analogWrite(in2, Speed);
+  analogWrite(in4, Speed);
+}
+
+void left() {
+  analogWrite(in3, Speed);
+  analogWrite(in2, Speed);
+}
+
+void right() {
+  analogWrite(in4, Speed);
+  analogWrite(in1, Speed);
+}
+
+void Stop() {
+  analogWrite(in1, 0);
+  analogWrite(in2, 0);
+  analogWrite(in3, 0);
+  analogWrite(in4, 0);
+}
+
+void brakeOn() {
+  buttonState = command;
+  if (buttonState != lastButtonState) {
+    if (buttonState == 'S') {
+      if (lastButtonState != buttonState) {
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, HIGH);
+        delay(brakeTime);
+        Stop();
+      }
+    }
+    lastButtonState = buttonState;
+  }
+}
+
+void brakeOff() 
+{
+}
